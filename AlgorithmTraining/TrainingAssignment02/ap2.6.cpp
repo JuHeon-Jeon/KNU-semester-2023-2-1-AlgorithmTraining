7fe5120c3c4ecc9c07f3ac913a466af3ee44f241
JuHeon-Jeon <114274896+JuHeon-Jeon@users.noreply.github.com>
2023-03-17 22:56:39 +0900

    ap2.6.cpp

--- AlgorithmTraining/TrainingAssignment02/ap2.6.cpp
+++ AlgorithmTraining/TrainingAssignment02/ap2.6.cpp
@@ -2,15 +2,63 @@
 using namespace std;
 
 typedef long long long2;
-int cnt;
+int cnt, king, kingCnt = 0;
 
-void colCon (long2 n)
+void colConPrint (long2 n)
 {
-    cnt = 0;
+    if (n == 1)
+        printf("1");
+    else if (n % 2)
+        {
+            printf("%d ", n);
+            colConPrint(n * 3 + 1);
+        }
+    else
+        {
+            printf("%d ", n);
+            colConPrint(n / 2);
+        }
+}
+
+void colConCnt (long2 n)
+{
     if (n == 1)
         cnt++;
     else if (n % 2)
-            colCon(n * 3 + 1);
+    {
+        cnt++;
+        colConCnt(n * 3 + 1);
+    }
     else
-            colCon(n / 2);
+    {
+        cnt++;
+        colConCnt(n / 2);
+    }
+}
+
+void colConCf (long2 n)
+{
+    cnt = 0;
+    colConCnt(n);
+    if (kingCnt <= cnt)
+        {
+            king = n;
+            kingCnt = cnt;
+        }
+}
+
+int main(void)
+{
+    int start, end;
+
+    scanf("%d %d", &start, &end);
+
+    king = start;
+    for (int i = start; i <= end; i++)
+        colConCf((long2)i);
+
+    printf("%d %d\n", king, kingCnt - 1);
+    colConPrint(king);
+
+    return 0;
 }
